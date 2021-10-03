c939cbcaecc2d23a9e555cb8fec90d552401a83a
Rafli2305 <abdulzero56@gmail.com>
2021-10-03 16:23:15 +0700

    Bot Rafli Cell Com

---
+++ message/mess.js
@@ -0,0 +1,17 @@
+module.exports.mess = {
+	        wait: 'PROSES...',
+			success: 'SEMOGA JADI PREMIUM LAGI',
+			wrongFormat: 'Format salah, coba liat lagi di menu',
+			error: {
+				api: 'APINYA HABIS, ISI ULANG DULU',
+				stick: 'ITU BUKAN STIKER TOLOL',
+				Iv: 'JANGAN NGASIH LINK GAJE TOD'
+			},
+			only: {
+				group: 'KHUSUS GRUP',
+				admin: 'KHUSUS ADMIN GRUP ',
+				premium: 'KHUSUS MEMBER PREMIUM',
+				owner: 'KHUSUS OWNER',
+				Badmin: 'BOT HARUS JADI ADMIN',
+			}
+		}
---
+++ runarfyudika.js
@@ -0,0 +1,92 @@
+const { WAConnection, Browsers } = require('@adiwajshing/baileys')
+const { color, bgcolor } = require('./lib/color')
+const fs = require("fs-extra")
+const figlet = require('figlet')
+const { uncache, nocache } = require('./lib/loader')
+const setting = JSON.parse(fs.readFileSync('./setting.json'))
+const welcome = require('./message/group')
+baterai = 'unknown'
+charging = 'unknown'
+
+//nocache
+require('./dha.js')
+nocache('../dha.js', module => console.log(color('[WATCH]', 'yellow'), color(`'${module}'`, 'cyan'), 'File is updated!'))
+require('./message/group.js')
+nocache('../message/group.js', module => console.log(color('[WATCH]', 'yellow'), color(`'${module}'`, 'yellow'), 'File is updated!'))
+
+const starts = async (dha = new WAConnection()) => {
+	dha.logger.level = 'warn'
+	console.log(color(figlet.textSync('BOT Rp 10.000', {
+		font: 'Standard',
+		horizontalLayout: 'default',
+		vertivalLayout: 'default',
+		width: 80,
+		whitespaceBreak: false
+	}), 'cyan'))
+	console.log(color('[SOURCE CODE BY GITHUB ARFYUDIKA]\n\n', 'orange'), color('\n======THANKS TO======\n•KURRXD\n•YUDHA PERDANA\n•SELURUH CREATOR BOT\n•SELURUH BUYER SCRIPT INI', 'yellow'))
+	console.log(color('\n\nNOT FOR SELL COMERSIL\nKALAU ADA YG MINTA SURUH CHAT ARFYUDIKA\nWA 6285282609948', 'pink'))
+	dha.browserDescription = ["RCC STORE", "Chrome", "3.0.0"];
+
+	// Menunggu QR
+	dha.on('qr', () => {
+		console.log(color('[', 'pink'), color('!', 'red'), color(']', 'pink'), color('SCAN SEKARANG! AUTO GENERATE 20 SEC'))
+	})
+
+	// Menghubungkan
+	fs.existsSync(`./${setting.sessionName}.json`) && dha.loadAuthInfo(`./${setting.sessionName}.json`)
+	dha.on('connecting', () => {
+		console.log(color('[ ARFYUDIKA ]', 'purple'), color('GENERATE QR CODE'));
+	})
+const spinner = { 
+  "interval": 120,
+  "frames": [
+    "A",
+    "AR",
+    "ARF",
+    "ARF Y",
+    "ARF YU",
+    "ARF YUD",
+    "ARF YUDI",
+    "ARF YUDIK",
+    "ARF YUDIKA",
+  ]}
+
+	//connect
+	dha.on('open', () => {
+		console.log(color('[ ARFYUDIKA ]', 'purple'), color('BOT AKTIF, SILAHKAN MENIKMATI'));
+	})
+
+	// session
+	await dha.connect({
+		timeoutMs: 30 * 1000
+	})
+	fs.writeFileSync(`./${setting.sessionName}.json`, JSON.stringify(dha.base64EncodedAuthInfo(), null, '\t'))
+
+	// Baterai
+	dha.on('CB:action,,battery', json => {
+		global.batteryLevelStr = json[2][0][1].value
+		global.batterylevel = parseInt(batteryLevelStr)
+		baterai = batterylevel
+		if (json[2][0][1].live == 'true') charging = true
+		if (json[2][0][1].live == 'false') charging = false
+		console.log(json[2][0][1])
+		console.log('Baterai : ' + batterylevel + '%')
+	})
+	global.batrei = global.batrei ? global.batrei : []
+	dha.on('CB:action,,battery', json => {
+		const batteryLevelStr = json[2][0][1].value
+		const batterylevel = parseInt(batteryLevelStr)
+		global.batrei.push(batterylevel)
+	})
+
+	// welcome
+	dha.on('group-participants-update', async (anu) => {
+		await welcome(dha, anu)
+	})
+
+	dha.on('chat-update', async (message) => {
+		require('./dha.js')(dha, message)
+	})
+}
+
+starts()
