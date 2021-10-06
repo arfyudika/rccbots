51210569acc0d5330d9d96fbb56b8bb192f93f00
arfyudika <62383431+Rafli2305@users.noreply.github.com>
2021-10-06 22:51:08 +0700

    Add files via upload

--- dha.js
+++ dha.js
@@ -2190,7 +2190,6 @@ case 'tiktok':
              await dha.sendMessage(from, ini_buffer, image, { quoted: mek })
              break
        case 'nhentaipdf':
-             if (!isPremium) return reply(mess.only.premium)
              if (args.length == 0) return reply(`Usage: ${prefix + command} query\nExample: ${prefix + command} 317986`)
              try {
              reply(mess.wait)
@@ -2214,15 +2213,14 @@ break
               ini_txt += `Title Native : ${get_result.title_native}\n`
               ini_txt += `Read Online : ${get_result.read}\n`
               ini_txt += `Parodies : ${get_result.parodies}\n`
-              get_info = result_info
-              ini_txt += `Character : ${get_info.characters.join(", ")}\n`
-              ini_txt += `Tags : ${get_info.tags.join(", ")}\n`
-              ini_txt += `Artist : ${get_info.artists}\n`
-              ini_txt += `Group : ${get_info.groups}\n`
-              ini_txt += `Languager : ${get_info.languages(", ")}\n`
-              ini_txt += `Categories : ${get_info.categories}\n`
-              ini_txt += `Pages : ${get_info.pages}\n`
-              ini_txt += `Uploaded : ${get_info.uploaded}\n`
+              ini_txt += `Character : ${get_result.characters.join(", ")}\n`
+              ini_txt += `Tags : ${get_result.tags.join(", ")}\n`
+              ini_txt += `Artist : ${get_result.artists}\n`
+              ini_txt += `Group : ${get_result.groups}\n`
+              ini_txt += `Languager : ${get_result.languages(", ")}\n`
+              ini_txt += `Categories : ${get_result.categories}\n`
+              ini_txt += `Pages : ${get_result.pages}\n`
+              ini_txt += `Uploaded : ${get_result.uploaded}\n`
               buffer = await getBuffer(data[0].thumb)
               await dha.sendMessage(from, thumbnail, image, { quoted: mek, caption: ini_txt })
               break
