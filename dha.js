40c27ffefcbfe26622f61025e1f0453eee7d89b6
arfyudika <62383431+Rafli2305@users.noreply.github.com>
2021-10-08 13:35:06 +0700

    Add files via upload

--- dha.js
+++ dha.js
@@ -3578,9 +3578,10 @@ break
               reply(`Follow Facebook Owner:\nfacebook.com/arfyudikaa`)
               break
        case 'datagame':
-              if (args.length < 1) return reply(`Ketik ${prefix}datagame [NAMA GAME]-[ID GAME]-[USERNAME]\n*NOTE:*\n*1.* _JANGAN LUPA BUKTI TRXNYA_\n*2.* _PESANAN AKAN_\n_LANSUNG DI PROSES_\n*3.*_JIKA PESANAN ANDA LOW_\n_MOHON BERSABAR_\nwa.me/6285282609948`) 
+              if (args.length < 1) return reply(`Ketik ${prefix}datagame\n${idgm}\n\n*NOTE:*\n*1.* _JANGAN LUPA BUKTI TRANSFER_\n*2.* _PESANAN AKAN LANSUNG DI PROSES_\n*3.* _JIKA PESANAN ANDA LOW_\n_MOHON BERSABAR_\nwa.me/6285282609948`) 
               teks = args.join(' ')
-              reply('Terima Kasih Telah Order, Jika Itu Sekedar Iseng Maka Akan Di Ban Oleh Bot!')
+              idgm = '-> NAMA GAME:\n-> ID GAME:\n-> USERNAME:'
+              reply('Terima kasih telah order,\npesananmu akan segera di proses')
               dha.sendMessage('6285282609948@s.whatsapp.net',`*Orderan:*\n ${teks}`, text)
               break
       case 'ping':
