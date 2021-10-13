bc7c93c64dc554ebdc4631b1f776b3672532207e
arfyudika <62383431+Rafli2305@users.noreply.github.com>
2021-10-13 15:35:10 +0700

    Update mess.js

--- message/mess.js
+++ message/mess.js
@@ -1,11 +1,11 @@
 module.exports.mess = {
-	        wait: 'PROSES...',
-			success: 'SEMOGA JADI PREMIUM LAGI',
+	        wait: 'Tunggu Sebentar..',
+			success: 'SEMOGA JADI PREMIUM',
 			wrongFormat: 'Format salah, coba liat lagi di menu',
 			error: {
 				api: 'APINYA HABIS, ISI ULANG DULU',
-				stick: 'ITU BUKAN STIKER TOLOL',
-				Iv: 'JANGAN NGASIH LINK GAJE TOD'
+				stick: 'ITU BUKAN STIKER',
+				Iv: 'JANGAN NGASIH LINK TIDAK JELAS'
 			},
 			only: {
 				group: 'KHUSUS GRUP',
